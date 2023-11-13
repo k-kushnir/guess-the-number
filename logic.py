890201ccbbc33202e3887dfa8f7a13199fa36a8a
kostiantyn <shaldon2009@gmail.com>
2023-11-13 15:50:18 +0200

    DRY optimisation

--- logic.py
+++ logic.py
@@ -3,71 +3,73 @@ import PySimpleGUI as sg
 import database as settings
 
 
-def game_logic():
-    while True:
-        number = random.randint(settings.start_number, settings.end_number)
-        attempts = settings.attempts
+class NumberGuessingGame:
+    def __init__(self):
+        self.number = 0
+        self.attempts = 0
+        self.window = None
 
-        # Зміна теми програми
+    def initialize_game(self):
+        self.number = random.randint(
+            settings.start_number, settings.end_number)
+        self.attempts = settings.attempts
+
         sg.theme(settings.theme)
 
         layout = [
             [sg.Text(
                 f"Я загадав число від {settings.start_number} до {settings.end_number}. У вас буде {settings.attempts} спроб, щоб вгадати його.")],
             [sg.Text("Введіть число: "), sg.InputText(key='attempt')],
-            # Додавання кнопки перезапуску гри
             [sg.Button("Вгадати", key='guess'), sg.Button("Вийти"),
              sg.Button("Грати знову", disabled=True, key='again')],
             [sg.Text("", size=(50, 1), key='result')]
         ]
 
-        window = sg.Window("Гра вгадай число", layout)
+        self.window = sg.Window("Гра вгадай число", layout)
 
+    def handle_guess_attempt(self, attempt):
+        if attempt == self.number:
+            self.window['result'].update("Ви вгадали! Ви перемогли!")
+            self.disable_guess_button()
+        elif attempt < self.number:
+            self.window['result'].update(
+                f"Загадане число більше. Спроб залишилось: {self.attempts}")
+        else:
+            self.window['result'].update(
+                f"Загадане число менше. Спроб залишилось: {self.attempts}")
+
+    def disable_guess_button(self):
+        self.window['guess'].update(disabled=True)
+        self.window['again'].update(disabled=False)
+
+    def run_game(self):
         while True:
-            event, values = window.read()
+            event, values = self.window.read()
+
             if event == sg.WIN_CLOSED or event == "Вийти":
-                window.close()
+                self.window.close()
                 break
 
             try:
                 attempt = int(values['attempt'])
             except ValueError:
-                window['result'].update("Будь ласка, введіть ціле число.")
+                self.window['result'].update("Будь ласка, введіть ціле число.")
                 continue
 
-            if attempt < settings.start_number or attempt > settings.end_number:
-                window['result'].update(
-                    f"Будь ласка, введіть число від {settings.start_number} до {settings.end_number}.")
-                continue
+            if settings.start_number <= attempt <= settings.end_number:
+                self.attempts -= 1
+                self.handle_guess_attempt(attempt)
 
-            attempts -= 1
-
-            if attempt == number:
-                window['result'].update("Ви вгадали! Ви перемогли!")
-                window['guess'].update(disabled=True)
-                window['again'].update(disabled=False)
-                break
-            elif attempt < number:
-                window['result'].update(
-                    f"Загадане число більше. Спроб залишилось: {attempts}")
+                if self.attempts == 0:
+                    self.window['result'].update(
+                        f"Гра закінчена. Загадане число було {self.number}.")
+                    self.disable_guess_button()
             else:
-                window['result'].update(
-                    f"Загадане число менше. Спроб залишилось: {attempts}")
+                self.window['result'].update(
+                    f"Будь ласка, введіть число від {settings.start_number} до {settings.end_number}.")
 
-            if attempts == 0:
-                window['result'].update(
-                    f"Гра закінчена. Загадане число було {number}.")
-                window['guess'].update(disabled=True)
-                window['again'].update(disabled=False)
-                break
-
-        while True:
-            event, values = window.read()
-            # логіка перезапуску гри
             if event == "again":
-                window.close()
-                game_logic()
-                return
-            if event == sg.WIN_CLOSED or event == "Вийти":
-                window.close()
+                self.window.close()
+                self.initialize_game()
+                self.run_game()
                 return
